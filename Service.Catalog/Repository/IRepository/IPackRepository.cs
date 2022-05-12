3ad2f650e7c7b2dbc18cfebd7882fc1f3d2348b6
julio sauri <jcastro@Axsistec.com>
2022-05-12 09:00:58 -0500

    PAck repository

--- Service.Catalog/Repository/IRepository/IPackRepository.cs
+++ Service.Catalog/Repository/IRepository/IPackRepository.cs
@@ -1,6 +1,15 @@
-﻿namespace Service.Catalog.Repository.IRepository
+﻿using Service.Catalog.Domain.Packet;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Service.Catalog.Repository.IRepository
 {
     public interface IPackRepository
     {
+        Task<Packet> GetById(int Id);
+        Task<List<Packet>> GetAll(string search);
+        Task Create(Packet pack);
+        Task Update(Packet pack);
+        Task<bool> IsDuplicate(Packet pack);
     }
 }
