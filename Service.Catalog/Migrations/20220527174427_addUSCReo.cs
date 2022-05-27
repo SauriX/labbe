ae8a0a6a28c1c3562f474a60803af507fbe6cd95
J. Raúl Hernández <msanchez@axsitec.com>
2022-05-27 13:00:08 -0500

    Agregar columnas en lealtades

---
+++ Service.Catalog/Migrations/20220527174427_addUSCReo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Service.Catalog.Migrations
+{
+    public partial class addUSCReo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "UsuarioCreoId",
+                table: "CAT_Lealtad",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "UsuarioModId",
+                table: "CAT_Lealtad",
+                type: "uniqueidentifier",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UsuarioCreoId",
+                table: "CAT_Lealtad");
+
+            migrationBuilder.DropColumn(
+                name: "UsuarioModId",
+                table: "CAT_Lealtad");
+        }
+    }
+}
