596fe39e67886f5142c710eb533842657bfc8d7d
Miguel Alejandro Farias Rocha <mfarias@axsistec.com>
2022-06-17 13:16:13 -0500

    [Feat]: Parametros de sistema => Configuracion con migracion

---
+++ Service.Catalog/Context/EntityConfiguration/Configuration/ConfigurationConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Catalog.Context.EntityConfiguration.Configuration
+{
+    public class ConfigurationConfiguration : IEntityTypeConfiguration<Domain.Configuration.Configuration>
+    {
+        public void Configure(EntityTypeBuilder<Domain.Configuration.Configuration> builder)
+        {
+            builder
+                .HasKey(x => x.Id);
+
+            builder
+                .Property(x => x.Valor)
+                .IsRequired(false)
+                .HasMaxLength(4000);
+
+            builder
+                .Property(x => x.Descripcion)
+                .IsRequired()
+                .HasMaxLength(4000);
+        }
+    }
+}
---
+++ Service.Catalog/Migrations/20220617181455_Create Table [CAT_Configuracion].cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Service.Catalog.Migrations
+{
+    public partial class CreateTableCAT_Configuracion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CAT_Configuracion",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    Descripcion = table.Column<string>(type: "nvarchar(4000)", maxLength: 4000, nullable: false),
+                    Valor = table.Column<string>(type: "nvarchar(4000)", maxLength: 4000, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CAT_Configuracion", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CAT_Configuracion");
+        }
+    }
+}
