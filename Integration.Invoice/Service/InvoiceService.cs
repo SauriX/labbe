93d52843817e507cd79d21713b2adffb4e2f140c
Miguel Alejandro Farias Rocha <mfarias@axsistec.com>
2022-07-12 08:48:51 -0500

    [Feat]: Factura => Primera prueba Facturapi, [Feat]: Expediente => Agregar clientes a appsetting

--- Integration.Invoice/Service/InvoiceService.cs
+++ Integration.Invoice/Service/InvoiceService.cs
@@ -1,11 +1,61 @@
-﻿using System;
+﻿using Facturapi;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 
 namespace Integration.Invoice.Service
 {
     public class InvoiceService
     {
+        public static async Task<string> Create()
+        {
+            var facturapi = new FacturapiClient("sk_test_jrKzRvqdg87nNoa6E0WBknQG7rJp1xlDwVyeGBAZ34");
+
+            try
+            {
+                //var facturapi = new FacturapiClient("sk_test_API_KEY");
+                var invoiceP =  facturapi.Invoice.CreateAsync(new Dictionary<string, object>
+                {
+                    ["customer"] = new Dictionary<string, object>
+                    {
+                        ["legal_name"] = "MIGUEL ALEJANDRO FARIAS ROCHA",
+                        ["email"] = "email@example.com",
+                        ["tax_id"] = "FARM960328BJ8",
+                        ["tax_system"] = "626",
+                        ["address"] = new Dictionary<string, object>
+                        {
+                            ["zip"] = "64102"
+                        }
+                    },
+                    ["items"] = new Dictionary<string, object>[]
+                  {
+    new Dictionary<string, object>
+    {
+      ["product"] = new Dictionary<string, object>
+      {
+        ["description"] = "Ukelele",
+        ["product_key"] = "60131324",
+        ["price"] = 1.00
+      }
+    }
+                  },
+                    ["payment_form"] = Facturapi.PaymentForm.DINERO_ELECTRONICO,
+                    ["folio_number"] = 914,
+                    ["series"] = "F"
+                });
+
+                var invoice = await invoiceP;
+                var a = invoice;
+
+                return "Hola";
+            }
+            catch (Exception e)
+            {
+
+                throw;
+            }
+        }
     }
 }
