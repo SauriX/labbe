faaafe4763f332b9300ff1ee01f7dfdb63972c88
Miguel Alejandro Farias Rocha <mfarias@axsistec.com>
2022-07-13 12:18:56 -0500

    [Feat]: General => Agregar swagger ui para QA

--- Service.Identity/Startup.cs
+++ Service.Identity/Startup.cs
@@ -152,7 +152,7 @@ namespace Service.Identity
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.IsDevelopment() || env.IsEnvironment("QA"))
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
--- Service.Report/Startup.cs
+++ Service.Report/Startup.cs
@@ -191,7 +191,7 @@ namespace Service.Report
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.IsDevelopment() || env.IsEnvironment("QA"))
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
