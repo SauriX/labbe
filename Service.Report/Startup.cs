e4a12f7430d9ed1d654e1a05b415fcd8c2ad58e0
Miguel Alejandro Farias Rocha <mfarias@axsistec.com>
2022-07-13 13:15:13 -0500

    [Feat]: General => Agregar Swagger a QA

--- Service.Report/Startup.cs
+++ Service.Report/Startup.cs
@@ -191,11 +191,23 @@ namespace Service.Report
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment() || env.IsEnvironment("QA"))
+            if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Service.Report v1"));
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Service.Report v1");
+                });
+            }
+            else if (env.IsEnvironment("QA"))
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("swagger/v1/swagger.json", "Service.Report v1");
+                    c.RoutePrefix = "";
+                });
             }
 
             app.UseCors("CorsPolicy");
