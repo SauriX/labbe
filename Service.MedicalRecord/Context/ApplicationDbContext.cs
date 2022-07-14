4e6deeff41dd2bee8473cee8695c835f74a69e8c
julio sauri <jcastro@Axsistec.com>
2022-07-14 16:37:41 -0500

    citas

--- Service.MedicalRecord/Context/ApplicationDbContext.cs
+++ Service.MedicalRecord/Context/ApplicationDbContext.cs
@@ -5,6 +5,7 @@ using Service.MedicalRecord.Domain.PriceQuote;
 using Service.MedicalRecord.Domain.TaxData;
 using Shared.Error;
 using System.Reflection;
+using Service.MedicalRecord.Domain.Appointments;
 
 namespace Service.MedicalRecord.Context
 {
@@ -15,8 +16,8 @@ namespace Service.MedicalRecord.Context
         public DbSet<TaxData> CAT_Datos_Fiscales { get; set; }
         public DbSet<MedicalRecordTaxData> Relacion_Expediente_Factura { get; set; }
         public DbSet<Request> CAT_Solicitud { get; set; }
-
-
+        public DbSet<AppointmentLab> CAT_Cita_Lab { get; set; }
+        public DbSet<AppointmentDom> CAT_Cita_Dom { get; set; }
         public DbSet<PriceQuote> CAT_Cotizaciones { get; set; }
         public DbSet<CotizacionStudy> cotizacionStudies { get; set; }
         protected override void OnModelCreating(ModelBuilder builder)
---
+++ Service.MedicalRecord/Domain/Appointments/AppointmentDom.cs
@@ -0,0 +1,33 @@
+﻿using Service.MedicalRecord.Domain.PriceQuote;
+using System;
+using System.Collections.Generic;
+
+namespace Service.MedicalRecord.Domain.Appointments
+{
+    public class AppointmentDom
+    {
+        public Guid Id { get; set; }
+        public Guid? ExpedienteId { get; set; }
+        public MedicalRecord.MedicalRecord? Expediente  { get; set; }
+        public string NombrePaciente    { get; set; }
+        //EquipoId
+        //UsuarioID
+        //RecolectorID
+        public string Estatus_Cita { get; set; }
+        public string Direccion { get; set; }
+        public string Celular { get; set; }
+        public string Genero { get; set; }
+        public string Email { get; set; }
+        public string WhatsApp { get; set; }
+        public string Indicaciones { get; set; }
+        //SolicitudId
+        public DateTime FechaCita { get; set; }
+        public DateTime HoraCita { get; set; }
+        public bool Activo { get; set; }
+        public Guid UsuarioCreoId { get; set; }
+        public DateTime FechaCreo { get; set; }
+        public Guid? UsuarioModId { get; set; }
+        public DateTime? FechaMod { get; set; }
+        public IEnumerable<CotizacionStudy>? Estudios { get; set; }
+    }
+}
