c5ca474f763a658136aea08bdaec35f08e1d80df
Juan Daniel Gonzalez Alejandre <dgonzalez@axsistec.com>
2022-07-26 00:07:17 -0500

    [Fix] Campos de tabla ContactStats y desarrollo de StudyStats

---
+++ Service.Report/Application/IApplication/IStudyStatsApplication.cs
@@ -0,0 +1,16 @@
+﻿using Service.Report.Dtos;
+using Service.Report.Dtos.StudyStats;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Service.Report.Application.IApplication
+{
+    public interface IStudyStatsApplication
+    {
+        Task<IEnumerable<StudyStatsDto>> GetByFilter(ReportFilterDto search);
+        Task<IEnumerable<StudyStatsChartDto>> GetCharByFilter(ReportFilterDto search);
+        Task<byte[]> DownloadReportPdf(ReportFilterDto search);
+    }
+}
---
+++ Service.Report/Dtos/StudyStats/StudyStatsDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Service.Report.Dtos.StudyStats
+{
+    public class StudyStatsDto
+    {
+        public string Solicitud { get; set; }
+        public string Paciente { get; set; }
+        public List<StudiesDto> Estudio { get; set; }
+        public int Edad { get; set; }
+        public string Sexo { get; set; }
+        public string Medico { get; set; }
+        public DateTime FechaEntrega { get; set; }
+        public string Estatus { get; set; }
+        public DateTime Fecha { get; set; }
+        public bool Parcialidad { get; set; }
+    }
+}
