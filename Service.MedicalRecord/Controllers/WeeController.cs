eef14aea14984f638009fda09779aa1e87f1db59
Miguel Alejandro Farias Rocha <mfarias@axsistec.com>
2022-08-23 13:12:58 -0500

    [Test]: WeeClinic => Prueba Login

---
+++ Service.MedicalRecord/Controllers/WeeController.cs
@@ -0,0 +1,20 @@
+﻿using Integration.WeeClinic;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Service.MedicalRecord.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [AllowAnonymous]
+    public class WeeController : ControllerBase
+    {
+        [HttpGet("login")]
+        public async Task<string> Login()
+        {
+            return await Base.Login();
+        }
+    }
+}
