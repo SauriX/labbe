41b72e7e723a8993633b6b51c9341e41046ef962
elhuguito <victor_hdez96@hotmail.com>
2022-08-31 12:16:25 -0500

    [Fix] Equipment response message

--- Service.Catalog/Application/EquipmentApplication.cs
+++ Service.Catalog/Application/EquipmentApplication.cs
@@ -140,7 +140,7 @@ namespace Service.Catalog.Application
 
             if (isDuplicate)
             {
-                throw new CustomException(HttpStatusCode.Conflict, Responses.Duplicated(Responses.Duplicated($"{code}")));
+                throw new CustomException(HttpStatusCode.Conflict, Responses.Duplicated($"{code}"));
             }
         }
     }
