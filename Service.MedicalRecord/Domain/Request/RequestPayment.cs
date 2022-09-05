ea17476cf410909fe2852cee94c273653dd74df5
Miguel Alejandro Farias Rocha <mfarias@axsistec.com>
2022-09-05 09:19:55 -0500

    [Feat]: Indicaciones => Cambio longitud de Descripcion para carga inicial

---
+++ Service.MedicalRecord/Domain/Request/RequestPayment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Service.MedicalRecord.Domain.Request
+{
+    public class RequestPayment
+    {
+        public Guid Id { get; set; }
+        public Guid SolicitudId { get; set; }
+        public virtual Request Solicitud { get; set; }
+        public int FormaPagoId { get; set; }
+        public string FormaPago { get; set; }
+        public int UsoCfdiId { get; set; }
+        public string NumeroCuenta { get; set; }
+        public DateTime FechaPago { get; set; }
+    }
+}
