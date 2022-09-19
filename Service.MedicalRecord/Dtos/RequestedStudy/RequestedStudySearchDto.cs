0df3133049b300359de28f7eb84f233f2f585d86
Juan Daniel Gonzalez Alejandre <dgonzalez@axsistec.com>
2022-09-19 11:43:29 -0500

    Bugs ortografia catalogos y busqueda registrar solicitud

--- Service.MedicalRecord/Dtos/RequestedStudy/RequestedStudySearchDto.cs
+++ Service.MedicalRecord/Dtos/RequestedStudy/RequestedStudySearchDto.cs
@@ -7,6 +7,8 @@ namespace Service.MedicalRecord.Dtos.RequestedStudy
 {
     public class RequestedStudySearchDto
     {
+        private List<byte> estatus;
+
         public List<Guid> SucursalId { get; set; }
         public List<string> MedicoId { get; set; }
         public List<string> CompañiaId { get; set; }
@@ -16,7 +18,7 @@ namespace Service.MedicalRecord.Dtos.RequestedStudy
         public List<int> Area { get; set; }
         public List<byte> TipoSolicitud { get; set; }
         public List<int> Procedencia { get; set; }
-        public List<byte> Estatus { get; set; }
+        public List<byte> Estatus { get => estatus.Any() ? estatus : new List<byte> { 2, 3 }; set => estatus = value; }
         public List<int> Estudio { get; set; }
         public List<Guid> SolicitudId { get; set; }
     }
