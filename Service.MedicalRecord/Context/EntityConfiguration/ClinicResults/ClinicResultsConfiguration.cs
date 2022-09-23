8923eb49048b1cffbfd3e49e8329ce23e2ce6227
Juan Daniel Gonzalez Alejandre <dgonzalez@axsistec.com>
2022-09-23 15:25:46 -0500

    ClinicResults migracion y repository

---
+++ Service.MedicalRecord/Context/EntityConfiguration/ClinicResults/ClinicResultsConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Service.MedicalRecord.Context.EntityConfiguration.ClinicResults
+{
+    public class ClinicResultsConfiguration : IEntityTypeConfiguration<Domain.ClinicResults>
+    {
+        public void Configure(EntityTypeBuilder<Domain.ClinicResults> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder
+                .HasOne(x => x.Estudio)
+                .WithMany()
+                .HasForeignKey(x => x.EstudioId);
+
+            builder
+               .HasOne(x => x.Solicitud)
+               .WithMany()
+               .HasForeignKey(x => x.SolicitudId);
+        }
+    }
+}
