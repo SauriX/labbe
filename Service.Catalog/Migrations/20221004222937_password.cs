59b138665631a0ef32770ecd1f2e32bcdde96932
julio sauri <jcastro@Axsistec.com>
2022-10-04 17:33:40 -0500

    password

---
+++ Service.Catalog/Migrations/20221004222937_password.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Service.Catalog.Migrations
+{
+    public partial class password : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "CAT_Medico",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "CAT_Medico");
+        }
+    }
+}
