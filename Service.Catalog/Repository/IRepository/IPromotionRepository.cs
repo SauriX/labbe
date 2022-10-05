83b7f2b9e0c32a713e61d8817294bede773b6213
Miguel Alejandro Farias Rocha <mfarias@axsistec.com>
2022-10-04 22:25:02 -0500

    [Control] Lista de precios => Obtener multiples promociones al consultar estudios y paquetes

--- Service.Catalog/Repository/IRepository/IPromotionRepository.cs
+++ Service.Catalog/Repository/IRepository/IPromotionRepository.cs
@@ -11,8 +11,8 @@ namespace Service.Catalog.Repository.IRepository
         Task<List<Promotion>> GetAll(string search);
         Task<Promotion> GetById(int id);
         Task<List<Promotion>> GetActive();
-        Task<PromotionStudy> GetStudyPromo(Guid priceListId, Guid branchId, int studyId);
-        Task<PromotionPack> GetPackPromo(Guid priceListId, Guid branchId, int packId);
+        Task<List<PromotionStudy>> GetStudyPromos(Guid priceListId, Guid branchId, Guid? doctorId, int studyId);
+        Task<List<PromotionPack>> GetPackPromos(Guid priceListId, Guid branchId, Guid? doctorId, int packId);
         Task Create(Promotion promotion);
         Task Update(Promotion promotion);
         Task<bool> IsDuplicate(Promotion promotion);
