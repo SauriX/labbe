2b2d9f36d6406b7f739a53d9aecfc1d7ae593b08
julio sauri <jcastro@Axsistec.com>
2022-10-07 12:25:10 -0500

    promotion

---
+++ Service.MedicalRecord/Dtos/PendingRecive/StatusDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Service.MedicalRecord.Dtos.PendingRecive
+{
+    public class StatusDto
+    {
+        public bool Created { get; set; }
+        public bool Smpling { get; set; }
+        public bool Route { get; set; }
+        public bool Entregado { get; set; }
+    }
+}
--- Service.MedicalRecord/Repository/RouteTrackingRepository.cs
+++ Service.MedicalRecord/Repository/RouteTrackingRepository.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using Service.MedicalRecord.Context;
 using Service.MedicalRecord.Domain.RouteTracking;
 using Service.MedicalRecord.Domain.TrackingOrder;
+using Service.MedicalRecord.Dtos.PendingRecive;
 using Service.MedicalRecord.Dtos.RouteTracking;
 using Service.MedicalRecord.Repository.IRepository;
 using System;
@@ -56,6 +57,33 @@ namespace Service.MedicalRecord.Repository
             await _context.SaveChangesAsync();
         }
 
-  
+        public async Task<List<TrackingOrder>> GetAllRecive(PendingSearchDto search) {
+
+            var routeTrackingList = _context.CAT_Seguimiento_Ruta.Include(x => x.Estudios).ThenInclude(x => x.Solicitud.Sucursal).AsQueryable();
+
+            if (search.Fecha != null)
+            {
+                routeTrackingList = routeTrackingList.
+                    Where(x => x.FechaCreo.Date == search.Fecha.Date );
+            }
+
+            if (search.Sucursal!=null && search.Sucursal.Count >0)
+            {
+                routeTrackingList = routeTrackingList.Where(x => search.Sucursal.Contains(x.SucursalOrigenId) && x.SucursalDestinoId==search.Sucursaldest);
+            }
+
+            if (!string.IsNullOrEmpty(search.Busqueda))
+            {
+                routeTrackingList = routeTrackingList.Where(x => search.Busqueda.Contains(x.Clave));
+            }
+            return await routeTrackingList.ToListAsync();
+        }
+
+        public async Task<RouteTracking> GetTracking(Guid Id) {
+            var routeTracking =  _context.Cat_PendientesDeEnviar.Include(x => x.Solicitud.Sucursal).FirstOrDefault(x=>x.SegumientoId==Id);
+            return routeTracking;
+        
+        }
+
     }
 }
