662cf1754163a6b7351961e0da049b30e02a749e
Miguel Alejandro Farias Rocha <mfarias@axsistec.com>
2022-10-25 11:54:02 -0500

    [Feat] Solicitudes => Actualizacion de Modelo RequestPayment

--- Service.MedicalRecord/Domain/Request/RequestPayment.cs
+++ Service.MedicalRecord/Domain/Request/RequestPayment.cs
@@ -2,15 +2,15 @@
 
 namespace Service.MedicalRecord.Domain.Request
 {
-    public class RequestPayment
+    public class RequestPayment : BaseModel
     {
         public Guid Id { get; set; }
         public Guid SolicitudId { get; set; }
         public virtual Request Solicitud { get; set; }
         public int FormaPagoId { get; set; }
         public string FormaPago { get; set; }
-        public int UsoCfdiId { get; set; }
         public string NumeroCuenta { get; set; }
         public DateTime FechaPago { get; set; }
+        public string UsuarioRegistra { get; set; }
     }
 }
