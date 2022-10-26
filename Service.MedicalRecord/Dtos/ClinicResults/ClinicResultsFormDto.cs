e5af4c74c31afc00831b0f206d08649f9c55bca6
julio sauri <jcastro@Axsistec.com>
2022-10-26 15:24:53 -0500

    unidades nombre

--- Service.MedicalRecord/Dtos/ClinicResults/ClinicResultsFormDto.cs
+++ Service.MedicalRecord/Dtos/ClinicResults/ClinicResultsFormDto.cs
@@ -23,6 +23,6 @@ namespace Service.MedicalRecord.Dtos.ClinicResults
         public byte Estatus { get; set; }
         public string DepartamentoEstudio { get; set; }
         public Guid UsuarioId { get; set; }
-        public string UnidadNombre { get; set; }
+        public string UnidadesNombre { get; set; }
     }
 }
