ea4246aa7101deb494e9588ca8d0e38d1d61229e
Miguel Alejandro Farias Rocha <mfarias@axsistec.com>
2022-11-08 11:40:06 -0600

    [Feat] Listas de trabajo => Nueva hoja si info de solicitud no cabe.

--- Integration.Pdf/Extensions/Extensions.cs
+++ Integration.Pdf/Extensions/Extensions.cs
@@ -1,4 +1,5 @@
 ﻿using MigraDoc.DocumentObjectModel;
+using MigraDoc.DocumentObjectModel.Tables;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +12,16 @@ namespace Integration.Pdf.Extensions
         public static void AddSpace(this Section section, double space = 5)
         {
             Paragraph p = section.AddParagraph();
+            p.Format.LineSpacingRule = LineSpacingRule.Exactly;
+            p.Format.LineSpacing = 0;
+            p.Format.SpaceAfter = Unit.FromPoint(space);
+        }
+
+        public static void AddSpace(this Row row, double space = 5)
+        {
+            var cell = row.Cells[0];
+
+            Paragraph p = cell.AddParagraph();
             p.Format.LineSpacingRule = LineSpacingRule.Exactly;
             p.Format.LineSpacing = 0;
             p.Format.SpaceAfter = Unit.FromPoint(space);
@@ -31,6 +42,35 @@ namespace Integration.Pdf.Extensions
             p.Format.LineSpacing = 0;
             p.Format.Borders.Bottom = new Border() { Width = Unit.FromPoint(width), Color = color ?? Colors.Black };
             p.Format.SpaceAfter = Unit.FromPoint(5);
+        }
+
+        public static void AddDivider(this Row parentRow, int width = 1, Color? color = null)
+        {
+            var parentCell = parentRow.Cells[0];
+
+            Table table = new Table();
+            table.Borders.Visible = false;
+            table.TopPadding = 0;
+            table.BottomPadding = 0;
+
+            float cellWidth = parentCell.Section.PageSetup.PageWidth - parentCell.Section.PageSetup.LeftMargin - parentCell.Section.PageSetup.RightMargin;
+
+            Column column = table.AddColumn();
+            column.LeftPadding = 0;
+            column.RightPadding = 0;
+            column.Width = cellWidth;
+
+            var row = table.AddRow();
+
+            var cell = row.Cells[0];
+
+            Paragraph p = cell.AddParagraph();
+            p.Format.LineSpacingRule = LineSpacingRule.Exactly;
+            p.Format.LineSpacing = 0;
+            p.Format.Borders.Bottom = new Border() { Width = Unit.FromPoint(width), Color = color ?? Colors.Black };
+            p.Format.SpaceAfter = Unit.FromPoint(5);
+
+            parentCell.Elements.Add(table);
         }
 
         public static void AddDivider(this HeaderFooter headerFooter, int width = 1, Color? color = null)
