c8731e0af3d60686b882292cdb314ab9d3d48047
Miguel Alejandro Farias Rocha <mfarias@axsistec.com>
2022-11-17 16:50:13 -0600

    [Feat] Solicitudes => Ya no se toma en cuenta IsCancel de WeeClinic para cancelar

--- Service.MedicalRecord/Application/RequestApplication.cs
+++ Service.MedicalRecord/Application/RequestApplication.cs
@@ -563,10 +563,10 @@ namespace Service.MedicalRecord.Application
                         throw new CustomException(HttpStatusCode.BadRequest, $"El estudio {study.Clave} no contiene información de WeeClinic, favor de contactar a su administrador de sistema");
                     }
 
-                    if (study.EstudioWeeClinic.IsCancel == 0)
-                    {
-                        throw new CustomException(HttpStatusCode.BadRequest, $"No es posible cancelar el estudio {study.Clave} a traves de WeeClinic");
-                    }
+                    //if (study.EstudioWeeClinic.IsCancel == 0)
+                    //{
+                    //    throw new CustomException(HttpStatusCode.BadRequest, $"No es posible cancelar el estudio {study.Clave} a traves de WeeClinic");
+                    //}
 
                     var weeCancelled = await _weeService.CancelService(study.EstudioWeeClinic.IdServicio, study.EstudioWeeClinic.IdNodo, branch.Clave);
 
