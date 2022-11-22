265fd807c3f15a21d8204113f0d9758126eb95ae
Miguel Alejandro Farias Rocha <mfarias@axsistec.com>
2022-11-22 10:41:45 -0600

    [Feat] Facturapi => Creacion de Dtos para facturar (Restauracion inicio)

---
+++ Integration.Invoice/Dtos/FacturapiProductDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Integration.Invoice.Dtos
+{
+    public class FacturapiProductDto
+    {
+        public string Descripcion { get; set; }
+        public string ClaveProductoSAT => "85121800"; // http://pys.sat.gob.mx/PyS/catPyS.aspx -> Laboratorios médicos (Servicios de análisis clínicos)
+        public decimal Precio { get; set; }
+        public bool IncliyeIVA => true;
+    }
+}
