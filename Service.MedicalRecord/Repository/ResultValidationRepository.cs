02e6a6e8819b625bccd1340c56a94d3bf88f550d
julio sauri <jcastro@Axsistec.com>
2022-12-07 16:20:08 -0500

    merge

--- Service.MedicalRecord/Repository/ResultValidationRepository.cs
+++ Service.MedicalRecord/Repository/ResultValidationRepository.cs
@@ -35,7 +35,7 @@ namespace Service.MedicalRecord.Repository
             var report = _context.CAT_Solicitud.Where(x => x.Estudios.Any(y => y.EstatusId == Status.RequestStudy.Capturado || y.EstatusId == Status.RequestStudy.Validado))
                 .Include(x => x.Expediente)
                 .Include(x => x.Medico)
-                .Include(x => x.Estudios.Where(y => y.EstatusId == Status.RequestStudy.Capturado ||y.EstatusId == Status.RequestStudy.Validado)).ThenInclude(x => x.Estatus)
+                .Include(x => x.Estudios.ThenInclude(x => x.Estatus)
                 .Include(x => x.Sucursal)
                 .Include(x => x.Compañia)
                 .AsQueryable();
