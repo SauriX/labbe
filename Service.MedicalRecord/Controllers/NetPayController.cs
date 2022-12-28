a20675cbed81b0308b8752ced320995de67b49c6
Miguel Alejandro Farias Rocha <mfarias@axsistec.com>
2022-12-28 09:39:50 -0600

    [Feat] NetPay => Controlador de prueba para configuracion de terminal

---
+++ Service.MedicalRecord/Controllers/NetPayController.cs
@@ -0,0 +1,35 @@
+﻿using DocumentFormat.OpenXml.Office2010.ExcelAc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Service.MedicalRecord.Controllers
+{
+    public class TestNetPay
+    {
+        public string FolioNumber { get; set; }
+        public string InternalNumber { get; set; }
+        public string TableId { get; set; }
+        public List<Dictionary<object, object>> ListOfPays { get; set; }
+        public string TipTotalAmount { get; set; }
+        public string TotalAmount { get; set; }
+    }
+
+    [AllowAnonymous]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NetPayController : ControllerBase
+    {
+        [HttpPost("test")]
+        public async Task<object> GetData(TestNetPay test)
+        {
+            var a = test;
+
+            return new { Code = "00", Message = "Recibido" };
+        }
+    }
+}
+
+// http://nubeqa.netpay.com.mx:3334
