e219a99633cfca5d5e8d2328a099bf8e560058e5
elhuguito <victor_hdez96@hotmail.com>
2022-12-30 09:36:40 -0600

    [Feat] InvoiceCompany se añadio la informacion de detalles de factura

---
+++ Service.MedicalRecord/Migrations/20221227172912_Add_Series_Table.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Service.MedicalRecord.Migrations
+{
+    public partial class Add_Series_Table : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
