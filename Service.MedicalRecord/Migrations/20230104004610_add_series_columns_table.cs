095eba50349dae0314379bff3465e2fe21ab09ae
elhuguito <victor_hdez96@hotmail.com>
2023-01-03 18:50:40 -0600

    [Feat] Invoice => se añadio la tabla de series

---
+++ Service.MedicalRecord/Migrations/20230104004610_add_series_columns_table.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Service.MedicalRecord.Migrations
+{
+    public partial class add_series_columns_table : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Series",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Clave = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    EsFacturaORecibo = table.Column<bool>(type: "bit", nullable: false),
+                    SucursalId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UsuarioCreoId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FechaCreo = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UsuarioModificoId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    FechaModifico = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Series", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Series");
+        }
+    }
+}
