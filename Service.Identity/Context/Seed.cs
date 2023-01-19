1b3fdcd5fd44e6614a4f756d18ffc11deec0adb5
Juan Daniel Gonzalez Alejandre <dgonzalez@axsistec.com>
2023-01-19 10:53:16 -0600

    Correccion Menu Orden 3000

--- Service.Identity/Context/Seed.cs
+++ Service.Identity/Context/Seed.cs
@@ -166,11 +166,10 @@ namespace Service.Identity.Context
 
                 // Resultados -> Orden 3000
                 new Menu(37, null, "Resultados", "results", "results", 3000), // Ultimo
-                new Menu(30, 38, "Captura de Resultados (Clínicos)", "clinicResults", "clinicResults", 3000),
-                //new Menu(36, 38, "Validación de resultados", "resultValidation", "resultValidation", 3010),
-                new Menu(36, 38, "Liberación de resultados", "relaseResult", "relaseResult", 3020),
-                new Menu(35, 38, "Listas de trabajo", "worklist", "worklists", 3030),
-                new Menu(31, 38, "Tablas de captura de resultados", "massSearch", "massResultSearch", 3040),
+                new Menu(30, 37, "Captura de Resultados (Clínicos)", "clinicResults", "clinicResults", 3000),
+                new Menu(36, 37, "Liberación de resultados", "relaseResult", "relaseResult", 3010),
+                new Menu(35, 37, "Listas de trabajo", "worklist", "worklists", 3020),
+                new Menu(31, 37, "Tablas de captura de resultados", "massSearch", "massResultSearch", 3030),
 
                 // Reportes -> Orden 4000
                 new Menu(22, null, "Reportes" , "report", "reports", 4000),
