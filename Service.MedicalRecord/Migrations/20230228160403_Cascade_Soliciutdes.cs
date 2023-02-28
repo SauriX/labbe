00aa598287e572a2cda5c124ef2766c2072cb04d
Miguel Farias <mike_fa96@hotmail.com>
2023-02-28 10:16:50 -0600

    [Fix] Observaciones cambio de compa;ia y eliminar solicitud

---
+++ Service.MedicalRecord/Migrations/20230228160403_Cascade_Soliciutdes.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Service.MedicalRecord.Migrations
+{
+    public partial class Cascade_Soliciutdes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Relacion_Solicitud_Pago_CAT_Solicitud_SolicitudId",
+                table: "Relacion_Solicitud_Pago");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Relacion_Solicitud_Pago_CAT_Solicitud_SolicitudId",
+                table: "Relacion_Solicitud_Pago",
+                column: "SolicitudId",
+                principalTable: "CAT_Solicitud",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Relacion_Solicitud_Pago_CAT_Solicitud_SolicitudId",
+                table: "Relacion_Solicitud_Pago");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Relacion_Solicitud_Pago_CAT_Solicitud_SolicitudId",
+                table: "Relacion_Solicitud_Pago",
+                column: "SolicitudId",
+                principalTable: "CAT_Solicitud",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
